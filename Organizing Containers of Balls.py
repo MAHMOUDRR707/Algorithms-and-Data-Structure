acd4f8caa529d4e6ab220b8ac0ac0a0eb4597039
Mahmoud Reda <55753782+MAHMOUDRR707@users.noreply.github.com>
2021-09-25 18:08:31 +0200

    Day 31

---
+++ Organizing Containers of Balls.py
@@ -0,0 +1,40 @@
+import math
+import os
+import random
+#porblem link : https://www.hackerrank.com/challenges/organizing-containers-of-balls/problem
+#time : O(n)
+
+import re
+import sys
+
+#
+# Complete the 'organizingContainers' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts 2D_INTEGER_ARRAY container as parameter.
+#
+
+def organizingContainers(container):
+    # Write your code here
+    if sorted([sum(i)  for i in container]) == sorted([sum(i) for i in zip(*container)]):
+        return "Possible" 
+    else: return "Impossible"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        container = []
+
+        for _ in range(n):
+            container.append(list(map(int, input().rstrip().split())))
+
+        result = organizingContainers(container)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
